38107807deb91349cce60b491cb748f5dfcbfba6
Stavros Iliopoulos <steveiliop56@gmail.com>
2023-05-13 18:17:54 +0300

    Added communication layer manual and fixed accesibility issues

--- ui/static/ui/script.js
+++ ui/static/ui/script.js
@@ -2,6 +2,24 @@ const chatBox = document.querySelector('.chat-box');
 const form = document.querySelector('form');
 const textInput = document.getElementById('text');
 const clearBtn = document.getElementById('clear-btn');
+const cookieNotification = document.getElementById('cookie-notification');
+const cookieAccept = document.getElementById('cookie-button');
+
+// Check if the notification has been displayed before
+if (!localStorage.getItem('cookie-notification-displayed')) {
+  // Display the notification
+  cookieNotification.style.display = 'block';
+
+  // Add an event listener to the "Accept" button
+  cookieAccept.addEventListener('click', () => {
+    // Hide the notification
+    cookieNotification.style.display = 'none';
+
+    // Set the flag indicating that the notification has been displayed
+    localStorage.setItem('cookie-notification-displayed', true);
+  });
+}
+
 
 // Function to add a user message to the chat box
 const addUserMessage = (text) => {
@@ -28,13 +46,23 @@ const generateSessionId = () => {
   return Math.floor(Math.random() * 1000000);
 };
 
+// Function to get or generate session ID
+const getSessionId = () => {
+  let sessionId = window.sessionStorage.getItem('sessionId');
+  if (!sessionId) {
+    sessionId = generateSessionId();
+    window.sessionStorage.setItem('sessionId', sessionId);
+  }
+  return sessionId;
+}
+
 // Event listener for form submission
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const text = textInput.value;
   addUserMessage(text);
   textInput.value = '';
-  const sessionId = generateSessionId();
+  const sessionId = getSessionId();
   const url = `/get_message/?session_id=${sessionId}&text=${text}`;
   fetch(url)
   .then(response => response.json())
@@ -51,3 +79,11 @@ clearBtn.addEventListener("click", () => {
 function goToAdmin() {
   window.location.href = '/admin';
 }
+
+// Event listener for the toggle sidebar button
+const toggleSidebarBtn = document.getElementById('toggle-sidebar-btn');
+const sidebar = document.querySelector('.sidebar');
+
+toggleSidebarBtn.addEventListener('click', function() {
+  sidebar.classList.toggle('hidden');
+});
--- ui/views.py
+++ ui/views.py
@@ -2,17 +2,29 @@ from django.shortcuts import render, HttpResponse
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from colorama import Fore
 
 # Create your views here.
+
+com = "manual" # could also be discord for discord backend.
 
 def index(request):
     return render(request, "ui/index.html")
 
+def manual(text, session_id):
+    print(Fore.GREEN + f"Received message {text} with session id {session_id}!")
+    message = input(Fore.BLUE + "Please enter what you want to respond: " + Fore.RESET)
+    return message
+
 @csrf_exempt
 def get_message(request):
+    global com
     session_id = request.GET.get('session_id', '')
     text = request.GET.get('text', '')
-    message = "Hello, you sent a GET request with session id {} and text {}".format(session_id, text)
+    if com == "manual":
+        message = manual(text, session_id)
+    else:
+        pass
     data = {
         'message': message
     }
